4b66aae88e96293aa097ff4982deec157b7f0f11
surya narayanan <74855435+soor234@users.noreply.github.com>
2025-06-04 18:05:42 +0530

    Create client.py

---
+++ client.py
@@ -0,0 +1,23 @@
+ def application
+{
+  int app;
+ print("application")
+}
+hostname.application=new application()
+url.load("www.chatbot.com")
+url.open()
+}
+print("url")
+{
+  port(8045)
+  {
+application app =new application()
+load.port(8045)
+{
+  print("app development)
+        url.fetch("www.chatbot.com)
+                  {
+        os.version(2018);
+        }
+        }
+        
