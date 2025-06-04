171c44a987894f964cf0c70853b77e7b601be3d0
surya narayanan <74855435+soor234@users.noreply.github.com>
2025-06-04 21:12:14 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,14 @@
+def webapplication
+{
+  int generateotp,
+  generate otp =OTP += digits[math.floor(random.random() * 07)]
+otp.run()
+if(name==otp)
+{
+  print("12234566.input.boxsizelen="5)
+  else:
+  {
+  print("invalid otp")
+  }
+  }
+  
